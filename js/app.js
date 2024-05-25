81165fbd1b072932a52ffdb43de00823f6a0d54e
r1d3rzz <myintthwaykhine127@gmail.com>
2024-05-25 12:44:48 +0630

    curd process with pure js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,114 @@
-const sideMenuBtn = document.getElementById("sideMenuBtn");
-const sideMenu = document.getElementById("side-menu");
+const textInput = document.querySelector("#textInput");
+const addBtn = document.querySelector("#addBtn");
+const lists = document.querySelector("#lists");
+const totalCount = document.querySelector("#totalCount");
+const doneCount = document.querySelector("#doneCount");
 
-const listsGroup = document.querySelectorAll(".list");
-const skillCount = document.getElementById("skills-count");
+const updateTotalCount = () => {
+  totalCount.innerText = document.querySelectorAll(".singleList").length;
+};
 
-listsGroup.forEach((list) => {
-  list.addEventListener("click", (e) => {
-    list.classList.toggle("bg-slate-200");
+const updateDoneCount = () => {
+  const totalDoneCount = document.querySelectorAll(".singleList input:checked");
+  doneCount.innerText = totalDoneCount.length;
+};
 
-    let count = document.querySelectorAll(".list.bg-blue-200").length;
+const createNewList = () => {
+  if (textInput.value == "") return false;
+  lists.append(singleList(textInput.value));
+  textInput.value = null;
+  updateTotalCount();
+};
 
-    skillCount.innerText = count > 0 ? `( ${count} )` : "";
+const singleList = (currentValue) => {
+  const newList = document.createElement("div");
+  newList.classList.add("singleList", "duration-150");
+  const list = `<div
+  class="list border-2 border-black flex justify-between items-center px-3 py-2 mb-2"
+>
+  <div class="flex">
+    <input type="checkbox" class="me-2 accent-emerald-950" id="checkBtn"/>
+    <p class="font-medium currentText">${currentValue}</p>
+  </div>
+  <div class="flex">
+    <button class="border-2 border-black p-1.5 me-2" id="editBtn">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+        />
+      </svg>
+    </button>
+    <button class="border-2 border-black p-1.5" id="delBtn">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-4"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+        />
+      </svg>
+    </button>
+  </div>
+</div>`;
+  newList.innerHTML = list;
+
+  const checkBtn = newList.querySelector("#checkBtn");
+  const currentText = newList.querySelector(".currentText");
+  const editBtn = newList.querySelector("#editBtn");
+  const delBtn = newList.querySelector("#delBtn");
+
+  checkBtn.addEventListener("change", () => {
+    updateDoneCount();
+    currentText.classList.toggle("line-through");
+    newList.classList.toggle("opacity-20");
+    newList.classList.toggle("scale-95");
+    editBtn.classList.toggle("hidden");
   });
-});
 
-sideMenuBtn.addEventListener("click", () => {
-  sideMenu.classList.toggle("translate-x-full");
-});
+  editBtn.addEventListener("click", () => {
+    const newInputTag = document.createElement("input");
+    newInputTag.className =
+      "border-2 border-black px-1 py-0.5 focus-visible:outline-none";
+    currentText.after(newInputTag);
+    newInputTag.focus();
+    newInputTag.value = currentText.innerText;
+    currentText.classList.add("hidden");
+    editBtn.setAttribute("disabled", true);
+    checkBtn.setAttribute("disabled", true);
+
+    newInputTag.addEventListener("blur", () => {
+      currentText.classList.remove("hidden");
+      editBtn.removeAttribute("disabled");
+      checkBtn.removeAttribute("disabled");
+      newInputTag.classList.toggle("hidden");
+      currentText.innerText = newInputTag.value;
+    });
+  });
+
+  delBtn.addEventListener("click", () => {
+    if (window.confirm("are u sure?")) {
+      newList.remove();
+      updateTotalCount();
+      updateDoneCount();
+    }
+  });
+
+  return newList;
+};
+
+addBtn.addEventListener("click", createNewList);
