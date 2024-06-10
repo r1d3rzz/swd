08bbf76396dd2a57ff847232b9b4cdf01bb0f35c
r1d3rzz <myintthwaykhine127@gmail.com>
2024-06-10 18:47:26 +0630

    using vite for inserting lib eg.tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["index.html", "./js/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-        padauk: ["Padauk", "sans-serif"],
-        ubuntu: ["Ubuntu Sans", "sans-serif"],
-      },
-      colors: {
-        primary: {
-          100: "#cce4f0",
-          200: "#99c9e2",
-          300: "#66add3",
-          400: "#3392c5",
-          500: "#0077b6",
-          600: "#005f92",
-          700: "#00476d",
-          800: "#003049",
-          900: "#001824",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
